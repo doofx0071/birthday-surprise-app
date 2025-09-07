862eb3b9cde6ac2ee59eeee06f447fcf82f39b4b
doofx0071 <pplona746@gmail.com>
2025-09-08 00:21:05 +0800

    fix: resolve birthday-input AnimatedSparkleIcon import TypeScript error
    - Add missing AnimatedSparkleIcon import to birthday-input component
    - Fix 'Cannot find name AnimatedSparkleIcon' error in:
      * birthday-input.tsx (sparkle icon in form hint section)
    - Import from @/design-system/icons/animated-birthday-icons
    - Complete TypeScript error resolution for design system form components

--- src/design-system/components/forms/birthday-input.tsx
+++ src/design-system/components/forms/birthday-input.tsx
@@ -4,7 +4,7 @@ import * as React from 'react'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { cn } from '@/lib/utils'
-import { AnimatedHeartIcon } from '@/design-system/icons/animated-birthday-icons'
+import { AnimatedHeartIcon, AnimatedSparkleIcon } from '@/design-system/icons/animated-birthday-icons'
 
 interface BirthdayInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string
